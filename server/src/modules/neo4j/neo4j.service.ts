2c839ffa3077faa75271eb61f41a1b861cd0605b
Andrei Malykh <xekoro10@gmail.com>
2024-11-21 13:37:15 +0300

    Add neo4j-errors filter

--- server/src/modules/neo4j/neo4j.service.ts
+++ server/src/modules/neo4j/neo4j.service.ts
@@ -1,30 +1,31 @@
-import { Inject, Injectable, OnApplicationShutdown } from '@nestjs/common';
-import { NEO4J_CONFIG, NEO4J_DRIVER } from './neo4j.constants';
-import neo4j, {Driver, Session } from 'neo4j-driver';
+import {Inject, Injectable, OnApplicationShutdown} from '@nestjs/common';
+import {NEO4J_CONFIG, NEO4J_DRIVER} from './neo4j.constants';
+import neo4j, {Driver, Session} from 'neo4j-driver';
 import {Neo4jConfig} from "./neo4j-config.interface";
 
 @Injectable()
 export class Neo4jService implements OnApplicationShutdown {
-  constructor(
-    @Inject(NEO4J_CONFIG) private readonly config: Neo4jConfig,
-    @Inject(NEO4J_DRIVER) private readonly driver: Driver,
-  ) {}
+    constructor(
+        @Inject(NEO4J_CONFIG) private readonly config: Neo4jConfig,
+        @Inject(NEO4J_DRIVER) private readonly driver: Driver,
+    ) {
+    }
 
-  getReadSession(): Session {
-    return this.driver.session({
-      database: this.config.database,
-      defaultAccessMode: neo4j.session.READ,
-    });
-  }
+    getReadSession(): Session {
+        return this.driver.session({
+            database: this.config.database,
+            defaultAccessMode: neo4j.session.READ,
+        });
+    }
 
-  getWriteSession(): Session {
-    return this.driver.session({
-      database: this.config.database,
-      defaultAccessMode: neo4j.session.WRITE,
-    });
-  }
+    getWriteSession(): Session {
+        return this.driver.session({
+            database: this.config.database,
+            defaultAccessMode: neo4j.session.WRITE,
+        });
+    }
 
-  onApplicationShutdown() {
-    return this.driver.close();
-  }
+    onApplicationShutdown() {
+        return this.driver.close();
+    }
 }
