873bae0a75d1ffc98f33b6ed7d5d6f4c86a306b0
Andrei Malykh <xekoro10@gmail.com>
2024-11-21 13:52:51 +0300

    Add database clearing on application start

--- server/src/modules/neo4j/neo4j.service.ts
+++ server/src/modules/neo4j/neo4j.service.ts
@@ -1,14 +1,23 @@
-import {Inject, Injectable, OnApplicationShutdown} from '@nestjs/common';
+import {Inject, Injectable, OnApplicationShutdown, OnModuleInit} from '@nestjs/common';
 import {NEO4J_CONFIG, NEO4J_DRIVER} from './neo4j.constants';
 import neo4j, {Driver, Session} from 'neo4j-driver';
 import {Neo4jConfig} from "./neo4j-config.interface";
 
 @Injectable()
-export class Neo4jService implements OnApplicationShutdown {
+export class Neo4jService implements OnApplicationShutdown, OnModuleInit {
     constructor(
         @Inject(NEO4J_CONFIG) private readonly config: Neo4jConfig,
         @Inject(NEO4J_DRIVER) private readonly driver: Driver,
     ) {
+    }
+
+    async onModuleInit() {
+        const session = this.getWriteSession();
+        try {
+            await session.run(`MATCH (n) DETACH DELETE n`);
+        } finally {
+            await session.close();
+        }
     }
 
     getReadSession(): Session {
