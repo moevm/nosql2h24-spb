273c3a7b8b7d4cad73875fda25ceee3a3c9d01d3
GeorIsay <GeorIsay.personal@yandex.ru>
2024-12-16 10:33:43 +0300

    add start frontend

---
+++ frontend/components/IconMap.vue
@@ -0,0 +1,24 @@
+<template>
+  <img src="~/assets/icons/map.svg" alt="User" />
+</template>
+
+<!-- <style>
+.vuetify-logo {
+  height: 180px;
+  width: 180px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style> -->
+
+<script>
+export default {
+  name: 'icon-user',
+};
+</script>
---
+++ frontend/pages/signup.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="registration-card">
+        <v-card-title class="headline text-center d-flex justify-center">
+          Авторизация0
+        </v-card-title>
+        <v-card-text class="d-flex flex-column align-center">
+          <v-text-field
+            label="Имя"
+            outlined
+            class="mb-9"
+          ></v-text-field>
+          <v-text-field
+            label="Email"
+            outlined
+            class="mb-9"
+          ></v-text-field>
+          <v-btn
+            color="accent"
+            class="mb-9"
+          >
+            Зарегистрироваться
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'SignUpPage'
+}
+</script>
+
+<style scoped>
+.registration-card {
+  /* background-color: primary; */
+  border-radius: 20px;
+  padding: 27px;
+}
+</style>
