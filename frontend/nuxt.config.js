78a41154851c6e822a48b9f53b12c2838cb72113
GeorIsay <GeorIsay.personal@yandex.ru>
2024-12-22 03:02:33 +0300

    auth and sign in done

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -29,7 +29,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios.js'
+    '~/plugins/axios.js',
+    {src: '~/plugins/auth.js', mode: 'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
