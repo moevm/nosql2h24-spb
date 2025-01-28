bcc0269fc427cc728cb48d3bac97ba75128549d2
Andrei Malykh <xekoro10@gmail.com>
2025-01-28 23:01:38 +0300

    Implement db data export

--- server/src/modules/neo4j/neo4j.module.ts
+++ server/src/modules/neo4j/neo4j.module.ts
@@ -4,6 +4,7 @@ import {createDriver} from './neo4j.util';
 import {Neo4jConfig} from './neo4j-config.interface';
 import {NEO4J_CONFIG, NEO4J_DRIVER} from './neo4j.constants';
 import {ConfigModule} from '@nestjs/config';
+import { Neo4jController } from './neo4j-controller';
 
 @Module({})
 export class Neo4jModule {
@@ -12,6 +13,7 @@ export class Neo4jModule {
       module: Neo4jModule,
       global: true,
       imports: [ConfigModule],
+      controllers: [Neo4jController],
 
       providers: [
         {
